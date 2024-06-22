9a6612714dc5a35b43ce4ca422e580459bf8b9b9
Arian Farid <arian.blank.farid@gmail.com>
2024-06-22 18:41:52 -0400

    moves player code to module

---
+++ src/player.rs
@@ -0,0 +1,100 @@
+
+use bevy::{app::{App, Plugin}, prelude::*};
+use bevy::window::PrimaryWindow;
+use crate::{MainCamera, GameState};
+pub struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+       app
+       .add_systems(OnEnter(GameState::Playing), setup)
+       .add_systems(Update, move_user)
+       .add_systems(Update, rotate_user);
+    }
+}
+
+#[derive(Component, Debug)]
+struct Player;
+
+#[derive(Component, Debug)]
+struct Velocity {
+    x: f32,
+    y: f32
+}
+const VELOCITY_CAP: f32 = 300.;
+
+#[derive(Component, Debug)]
+struct Acceleration {
+    x: f32,
+    y: f32
+}
+const ACC_CAP: f32 = 20.;
+const USER_SPEED: f32 = 200.0;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("ship.png"),
+            transform: Transform::from_xyz(100., 0., 0.),
+            ..default()
+        },
+        Player,
+        Velocity {x: 0., y: 0.},
+        Acceleration {x: 0., y: 0.},
+    ));
+}
+
+fn move_user(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&mut Transform), With<Player>>,
+    time: Res<Time>,
+) {
+    let mut player_transform = query.single_mut();
+    let mut x_direction = 0.0;
+    let mut y_direction = 0.0;
+
+    // Todo: map to an optional user input
+    if keyboard_input.pressed(KeyCode::ArrowLeft) || keyboard_input.pressed(KeyCode::KeyA) {
+        x_direction -= 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::ArrowRight) || keyboard_input.pressed(KeyCode::KeyD) {
+        x_direction += 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::ArrowUp) || keyboard_input.pressed(KeyCode::KeyW) {
+        y_direction += 1.0;
+    }
+    if keyboard_input.pressed(KeyCode::ArrowDown) || keyboard_input.pressed(KeyCode::KeyS) {
+        y_direction -= 1.0;
+    }
+
+    let new_player_position_x =
+        player_transform.translation.x + x_direction * USER_SPEED * time.delta_seconds();
+    let new_player_position_y =
+        player_transform.translation.y + y_direction * USER_SPEED * time.delta_seconds();
+
+    player_transform.translation.x = new_player_position_x;
+    player_transform.translation.y = new_player_position_y;
+}
+
+fn rotate_user(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    q_windows: Query<&Window, With<PrimaryWindow>>,
+    q_camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+) { 
+    let (camera, camera_transform) = q_camera.single();
+    let mut player_transform = player_query.single_mut();
+    let player_translation = player_transform.translation.xy();
+
+    let cursor_pos = q_windows.single().cursor_position()
+    .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
+    .map(|ray| ray.origin.truncate());
+    match cursor_pos {
+       Some(v) => {
+        let to_mouse = (player_translation - v).normalize();
+        let rotate_to_mouse = Quat::from_rotation_arc(Vec3::Y, to_mouse.extend(0.));
+        player_transform.rotation = rotate_to_mouse;
+       },
+       None => {},
+    }
+}
